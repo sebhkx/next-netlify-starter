e0772c2cd310846135b951a6786be0914d7d0678
sebastianhkx <kxhong.2018@sis.smu.edu.sg>
2023-04-09 15:33:37 +0800

    Update index.js
    attempt of fix of className

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,10 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my site!" />
-        <p className="calendar">
+        <p className="description">
+          <html>
           <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FSingapore&showPrint=0&title=Seb's%20Calendar&src=c2ViaC5reEBnbWFpbC5jb20&src=cDVncGRzc2F1ZnVyNmhxYzBwMmpjajFpb2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230e61b9&color=%23a6000b" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
+          </html>
         </p>
       </main>
 
