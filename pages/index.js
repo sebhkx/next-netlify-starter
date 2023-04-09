0d9d441af2d7cf926fb4a014556580fa3eb99d62
sebastianhkx <kxhong.2018@sis.smu.edu.sg>
2023-04-09 15:42:16 +0800

    rollback

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          <html>
-          <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FSingapore&showPrint=0&title=Seb's%20Calendar&src=c2ViaC5reEBnbWFpbC5jb20&src=cDVncGRzc2F1ZnVyNmhxYzBwMmpjajFpb2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230e61b9&color=%23a6000b" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-          </html>
+          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
